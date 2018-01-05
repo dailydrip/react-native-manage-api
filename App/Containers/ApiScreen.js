5318589922a821e4183fc3c67a47e8be8a1cc606
Franzé Jr <franzejr@gmail.com>
2018-01-05 09:35:50 -0200

    Get image from knewter using just a button

---
+++ App/Containers/ApiScreen.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { connect } from "react-redux";
+import { FlatList, Image } from "react-native";
+import {
+  List,
+  ListItem,
+  Text,
+  View,
+  Container,
+  Header,
+  Title,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon
+} from "native-base";
+// import Icon from 'react-native-vector-icons/Ionicons'
+
+class ApiScreen extends React.Component {
+  _renderItem = ({ item }) => {
+    return (
+      <ListItem style={{ justifyContent: "space-between" }}>
+        <Text>{item.title}</Text>
+        <Text note>{item.description}</Text>
+      </ListItem>
+    );
+  };
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.navigate("DrawerOpen")}
+            >
+              <Icon name="ios-menu" />
+            </Button>
+          </Left>
+          <Body style={{ flex: 3 }}>
+            <Title>Api Screen</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Text>
+          ApiScreen
+        </Text>
+        <Button onPress={() => this.props.getUserAvatar("knewter")}>
+          <Text> Get knewter Avatar </Text>
+        </Button>
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={{
+            uri: this.props.avatar
+          }}
+        />
+      </Container>
+    );
+  }
+}
+const mapStateToProps = state => {
+  debugger;
+  return {
+    avatar: state.github.avatar
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getUserAvatar: username =>
+      dispatch({ type: "USER_REQUEST", username: username })
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(ApiScreen);
--- App/Navigation/NavigationDrawer.js
+++ App/Navigation/NavigationDrawer.js
@@ -3,17 +3,23 @@ import { DrawerNavigator } from "react-navigation";
 import ListviewExample from "../Containers/ListviewExample";
 import CardExample from "../Containers/CardExample";
 import DrawerContent from "../Containers/DrawerContent";
+import ApiScreen from "../Containers/ApiScreen";
 
 import styles from "./Styles/NavigationStyles";
 
-const NavigationDrawer = DrawerNavigator({
-		ListviewExample: { screen: ListviewExample },
-		CardExample: { screen: CardExample },
-	},
-	{
-		initialRouteName: "ListviewExample",
-		contentComponent: props => <DrawerContent {...props} />,
-	}
+const NavigationDrawer = DrawerNavigator(
+  {
+    ListviewExample: { screen: ListviewExample },
+    CardExample: { screen: CardExample },
+    ApiScreen: { screen: ApiScreen }
+  },
+  {
+    initialRouteName: "ListviewExample",
+    contentComponent: props => <DrawerContent {...props} />,
+    drawerOpenRoute: "DrawerOpen",
+    drawerCloseRoute: "DrawerClose",
+    drawerToggleRoute: "DrawerToggle"
+  }
 );
 
 export default NavigationDrawer;
